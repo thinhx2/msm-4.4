fe14d5f69d71d48c8b707c5d7abe11dd8b23b876
thinhx2 <nthinh1102@gmail.com>
2020-02-09 21:05:54 +0700

    fix bug

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -1044,8 +1044,6 @@ int qcom_batt_init(void)
 	INIT_DELAYED_WORK(&chip->status_change_work, status_change_work);
 	INIT_DELAYED_WORK(&chip->pl_taper_work, pl_taper_work);
 	INIT_WORK(&chip->pl_disable_forever_work, pl_disable_forever_work);
-	INIT_DELAYED_WORK(&chip->pl_awake_work, pl_awake_work);
-	INIT_DELAYED_WORK(&chip->fcc_step_update_work, fcc_step_update_work);
 
 	chip->fcc_votable = create_votable("FCC", VOTE_MIN,
 					pl_fcc_vote_callback,
